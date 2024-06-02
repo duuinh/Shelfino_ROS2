abfb904cd81b605311963a8157df73129ae6defe
Natnipa S <s.natnipa@gmail.com>
2024-06-02 14:28:18 +0000

    fix bug in comparing result

--- projects/src/orienteering_solver.cpp
+++ projects/src/orienteering_solver.cpp
@@ -3,6 +3,8 @@
 std::vector<int> find_optimal_path(double max_distance, std::vector<std::vector<double>> distance_matrix, std::vector<double> rewards) {
     int n_nodes_ = distance_matrix.size();
     std::vector<int> nodes(n_nodes_ - 2);
+
+    std::vector<PathValue> path_value_list;
     
     for (int i = 0; i < n_nodes_-2; ++i) {
         nodes[i] = i+1;
@@ -13,15 +15,14 @@ std::vector<int> find_optimal_path(double max_distance, std::vector<std::vector<
         combinations.push_back(nodes);
     } while ( std::next_permutation(nodes.begin(),nodes.end()) );
 
-    std::vector<int> best_path;
-    double best_path_reward = -1;
-    double best_path_distance = 0;
     
     for (int i = 0; i < combinations.size(); i++) {
         double total_distance = 0;
         double total_reward = 0;
         int current_node = 0;
         std::vector<int> path = {0};
+
+
         for (int j = 0; j < combinations[i].size(); j++) {
             double temp = total_distance + distance_matrix[current_node][combinations[i][j]] + distance_matrix[combinations[i][j]][n_nodes_ -1];
             if (temp > max_distance) {
@@ -36,20 +37,17 @@ std::vector<int> find_optimal_path(double max_distance, std::vector<std::vector<
         path.push_back(n_nodes_-1);
         total_distance += distance_matrix[current_node][n_nodes_-1];
         
-        if (total_reward >= best_path_reward) {
-            if (total_reward == best_path_reward && total_distance < best_path_distance) {
-                best_path = path;
-                best_path_reward = total_reward;
-                best_path_distance = total_distance;
-            } else {
-                best_path = path;
-                best_path_reward = total_reward;
-                best_path_distance = total_distance;
-            }
-        }
+        path_value_list.push_back(PathValue(path, total_distance, total_reward));
     }
     
-    std::cout << "best_path_reward: "<< best_path_reward << std::endl;
-    std::cout << "best_path_distance: "<< best_path_distance << std::endl;
-    return best_path;
+    std::sort(path_value_list.begin(), path_value_list.end(), [](const PathValue &p1, const PathValue &p2) {
+        if (p1.total_reward == p2.total_reward) {
+            return p1.total_distance < p2.total_distance;
+        }
+        return p1.total_reward > p2.total_reward;
+    });
+
+    std::cout << "best_path_reward: "<< path_value_list[0].total_reward << std::endl;
+    std::cout << "best_path_distance: "<< path_value_list[0].total_distance << std::endl;
+    return path_value_list[0].path;
 }
