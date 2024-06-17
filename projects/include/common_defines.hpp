8f7fadf3991bcde7c0a1456a6920320b9aa2efe0
Natnipa S <s.natnipa@gmail.com>
2024-06-17 04:16:20 +0000

    reduce max time

--- projects/include/common_defines.hpp
+++ projects/include/common_defines.hpp
@@ -4,4 +4,4 @@
 
 #define ROBOT_RADIUS    0.4             // radius of robot occupation [m]
 #define ROBOT_VELOCITY  0.2             // velocity of robot [m/s]
-#define MAX_TIME        300             // maximum time budget [second]
+#define MAX_TIME        120             // maximum time budget [second]
